e71d2a55dba1758afeb7356125b7c3c88e6bff11
weather98 <112868136+weather98@users.noreply.github.com>
2023-01-17 22:26:41 +0900

    3입력 업데이트 추가+ index

--- app.py
+++ app.py
@@ -10,7 +10,18 @@ app = Flask("board")
 
 @app.route("/")
 def index():
-    return render_template("index.html") #templates 폴더의 index.html 을 렌더링
+    question = []
+    with sql.connet("database.db") as con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM question")
+        for data in cur.fetchall():
+            question = {} #튜플로 리스트
+            question['id']=data[0]
+            question['month']=data[1]
+            question['day']=data[2]
+            question['title']=data[3]
+            question.append(question)
+    return render_template("index.html", question) #templates 폴더의 index.html 을 렌더링
 
 @app.route("/create", methods=['POST'])
 def create():
@@ -21,6 +32,15 @@ def create():
         cur = con.cursor()
         cur.execute("INSERT INTO question (month, day, title) VALUES (?, ?. ?)", (month, day, title))
     return redirect("/") #해당페이지로 다시 접속
+
+@app.route("/update", methods=['POST'])
+def update():
+    uid = request.form['id']
+    title = request.form['title']
+    with sql.connet('database.db') as con:
+        cur = con.cursor()
+        cur.execute("UPDATE question SET title=? WHERE id=?", (title, uid))
+    return redirect("/")
 
 app.run(host='127.0.0.1', port='5000')
 
